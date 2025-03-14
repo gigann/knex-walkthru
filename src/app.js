4ebc82789a5e6c2b7072cc9bce2b3120bddf57a6
gigann <issacgann@gmail.com>
2025-03-14 20:21:11 +0100

    created /pets endpoint

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
+const knex = require('knex')(require('../knexfile.js')['development']);
+
 app.get('/', (req, res) => {
-  res.send('Hello World! App up and running.');
+    res.send('Hello World! App up and running.');
 });
 
 app.listen(port, () => {
     console.log(`Knex and Express app listening at http://localhost:${port}`);
+});
+
+// create endpoint
+app.get('/pets', (req, res) => {
+    knex('pet')
+        .select('*')
+        .then(pets => {
+            let petNames = pets.map(pet => pet.name);
+            res.json(petNames);
+        })
 });
