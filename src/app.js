5d3609697b7564cc45027f1f2c89b4f16bc022e7
gigann <issacgann@gmail.com>
2025-03-14 20:24:47 +0100

    added more to app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 3000;
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV || 'development']);
 
-const knex = require('knex')(require('../knexfile.js')['development']);
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World! App up and running.');
-});
-
-app.listen(port, () => {
-    console.log(`Knex and Express app listening at http://localhost:${port}`);
 });
 
 // create endpoint
 app.get('/pets', (req, res) => {
     knex('pet')
         .select('*')
-        .then(pets => {
-            let petNames = pets.map(pet => pet.name);
-            res.json(petNames);
-        })
+        .then(data => res.status(200).json(data))
+        .catch(err => res.status(404).json({ message: 'The data you are looking for could not be found.' }));
+});
+
+app.listen(port, () => {
+    console.log(`Knex and Express app listening at http://localhost:${port}`);
 });
